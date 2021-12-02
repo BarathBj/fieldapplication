b0ac7c923f5394a34f37a7bc7e13afba8e22ecc5
BarathBj <94238296+BarathBj@users.noreply.github.com>
2021-12-02 15:35:25 +0530

    Add files via upload

---
+++ fields.rb
@@ -0,0 +1,14 @@
+fields:
+   -> create employe name
+   -> edit employe age
+   -> view employe gender
+   -> delete employe designation
+
+
+
+FIELD:
+  
+  name: name of the employe(string)
+  age : age of the employe(decimal)
+  gender: gender of the employe(string)
+  designation role: designation role of the employe(string) 
